e4c87d3a285f8f2340919ceddd764cfb0b2a7c98
Yeeun <22000546@handong.edu>
2021-09-11 17:05:49 +0900

    Implement getters and setters, toString() in Fruit class

--- src/main/java/com/example/lab2/Fruit.java
+++ src/main/java/com/example/lab2/Fruit.java
@@ -9,7 +9,43 @@ public class Fruit {
 	Fruit() {
 		
 	}
-	
-	
+
+	public Fruit(int no, String name, int price) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Fruit [no=" + no + ", name=" + name + ", price=" + price + "]";
+	}
 
 }
+
+
