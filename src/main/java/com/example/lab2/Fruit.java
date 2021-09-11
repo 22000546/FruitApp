52c0ee60b4a2442112fcb776ec071960a8a38a8a
Yeeun <22000546@handong.edu>
2021-09-11 17:10:44 +0900

    Implement FruitComparator/FruitComparatorDesc

--- src/main/java/com/example/lab2/Fruit.java
+++ src/main/java/com/example/lab2/Fruit.java
@@ -1,5 +1,7 @@
 package com.example.lab2;
 
+import java.util.Comparator;
+
 public class Fruit {
 	
 	int no;
@@ -48,4 +50,20 @@ public class Fruit {
 
 }
 
+class FruitComparator implements Comparator<Fruit> {
 
+	@Override
+	public int compare(Fruit o1, Fruit o2) {
+		return o1.name.compareTo(o2.name);
+	}
+
+}
+
+class FruitComparatorDesc implements Comparator<Fruit> {
+
+	@Override
+	public int compare(Fruit o1, Fruit o2) {
+		return o2.name.compareTo(o1.name);
+	}
+	
+}
